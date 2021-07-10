0f622954edde03722a9a6e68fbad1327c0d06ee1
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2021-07-10 14:10:31 -0400

    added missing gstreamer plugin

--- installations.sh
+++ installations.sh
@@ -1,3 +1,4 @@
 # Install gstreamer
-sudo apt-get install libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev libgstreamer-plugins-bad1.0-dev libgstrtspserver-1.0-dev 
+sudo apt-get install libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev libgstreamer-plugins-bad1.0-dev libgstrtspserver-1.0-dev gstreamer1.0-plugins-bad
+
 
