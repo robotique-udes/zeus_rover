028085fe9ce6b9323f5bac01afc428fff514ec29
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2021-07-22 12:09:09 -0400

    mapviz launch, config, and google maps mapproxy

--- installations.sh
+++ installations.sh
@@ -1,4 +1,12 @@
+#!/usr/bin/env bash
+
+sudo apt update
+
 # Install gstreamer
 sudo apt-get install libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev libgstreamer-plugins-bad1.0-dev libgstrtspserver-1.0-dev gstreamer1.0-plugins-bad
 
+# Install docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
+# Setup mapproxy
+mkdir ~/mapproxy
